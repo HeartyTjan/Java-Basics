60e7bdadc0e66a583c242321ed45d8be197ebf4e
dev0xeb <clintonayomide96@gmail.com>
2024-09-01 09:42:49 +0100

    Classwork

---
+++ Class_Work/FunctionTrianglePattern2.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+public class FunctionTrianglePattern2{
+
+
+	public static void main(String... args){
+	
+		Scanner receiver = new Scanner(System.in);
+		System.out.print("Enter your triangle size: ");
+		int size = receiver.nextInt();
+		
+		printNumberOfTriangleSize(size);
+	}
+
+	public static void printNumberOfTriangleSize(int number){
+		
+		printIncreasingTriangle(number);
+		printDecreasingTriangle(number);
+		
+	}
+
+	public static void printDecreasingTriangle(int number){
+		
+		for (int count =1; count <=number; count++){
+
+			for (int secondCount = count; secondCount <=number; secondCount++){
+			
+				System.out.print("* ~");	
+			}
+			System.out.println();
+
+			for (int secondCount = count; secondCount <=number; secondCount++){
+				System.out.print("|  ");	
+			}
+
+			System.out.println();			
+		}
+	}
+
+	public static void printIncreasingTriangle(int number){
+
+		for (int count =1; count < number; count++){
+
+			
+			for (int secondCount = 1; secondCount <= count; secondCount++){
+			
+				System.out.print("* ~");	
+			}
+			System.out.println();	
+			
+			for (int secondCount = 1; secondCount <= count; secondCount++){
+				System.out.print("|  ");	
+			}
+			System.out.println();
+		}
+	}
+
+}
