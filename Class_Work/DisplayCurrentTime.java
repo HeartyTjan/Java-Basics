9e8dedf5f751a168e0b9a9686a9ec6819030c369
dev0xeb <clintonayomide96@gmail.com>
2024-09-06 20:13:25 +0100

    classwork

---
+++ Class_Work/DisplayCurrentTime.java
@@ -0,0 +1,25 @@
+public class DisplayCurrentTime{
+
+	public static void main(String... args){
+
+		long milliSeconds = System.currentTimeMillis();
+		
+		long totalSeconds =  milliSeconds / 1000;
+	
+		long currentSeconds = totalSeconds % 60;
+	
+		long totalMinutes = totalSeconds / 60;
+
+		long currentMinutes = totalMinutes % 60;
+
+		long totalHours = totalMinutes / 60;
+
+		long currentHour = totalHours % 24;
+		
+
+		System.out.print("The is current time is " +  currentHour + " :" + currentMinutes + " :" + currentSeconds);
+
+
+	}
+
+}
