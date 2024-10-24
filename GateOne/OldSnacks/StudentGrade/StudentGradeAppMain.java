e60d7a10c9bc0346f511ed4957e340121142f01a
HeartyTjan <salamitijani02@gmail.com>
2024-10-24 16:14:04 +0100

    Update

--- GateOne/OldSnacks/StudentGrade/StudentGradeAppMain.java
+++ GateOne/OldSnacks/StudentGrade/StudentGradeAppMain.java
@@ -9,28 +9,38 @@ public class StudentGradeAppMain{
 	        StudentGradeApp tester = new StudentGradeApp();
 
 		int numbeberOfStudent = 0;
-		int numbeberOfSubject = 0;
+		int numbeberOfSubject = 0; 
+
+		boolean stateOfBeing = true;
+
 	  	 try{
 			System.out.printf("How many student do you have? : ");
 			numbeberOfStudent = receiver.nextInt();
 		
 			System.out.printf("%n%nHow many subject do they offer? : ");
 			numbeberOfSubject = receiver.nextInt();
+
 		}catch(InputMismatchException e){
 
 			System.out.print(e);
+			stateOfBeing = false;
+		}
+	
+		if (stateOfBeing){
+
+			System.out.printf("%nSaving >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>%n");
+
+			System.out.printf("%nSaved successfuly%n");
+
+			try{
+				 tester.displayStudentScores(numbeberOfStudent,numbeberOfSubject);
+			}catch(Exception e){
+
+				System.out.print(e);
+			}
 
 		}
-		System.out.printf("%nSaving >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>%n");
 
-		System.out.printf("%nSaved successfuly%n");
-
-		try{
-			 tester.displayStudentScores(numbeberOfStudent,numbeberOfSubject);
-		 }catch(Exception e){
-
-			System.out.print(e);
-		}
-
+		
 	}
 }
